76c1932210c3be999ceea528489bdf0097e9b497
dittekoustrup <dittekoustrup@outlook.com>
2023-10-19 14:06:32 +0200

    Navigation setup
    Created the scss files for navigation, variables and global styling. Added menu points and started styling.
    
    Co-Authored-By: KasperPtak <60694854+KasperPtak@users.noreply.github.com>
    Co-Authored-By: Sophiangu <146834625+Sophiangu@users.noreply.github.com>
    Co-Authored-By: alvihelene <95623140+alvihelene@users.noreply.github.com>
    Co-Authored-By: soklope <95622881+soklope@users.noreply.github.com>

--- webdock-app/src/layout/navigation/Navigation.jsx
+++ webdock-app/src/layout/navigation/Navigation.jsx
@@ -1,16 +1,29 @@
-import { Link } from "react-router-dom"
-import "./Navigation.scss"
+import { Link } from "react-router-dom";
+import "./Navigation.scss";
+import logo from "../../content/images/logo_200x200.png";
 
 export default function Navigation() {
-    return ( 
-        <div>
-            <Link>Webdock logo</Link>
-            <Link>Roadmap</Link>
-            <Link>Feature Request</Link>
-            <Link>Profil</Link>
-            <div>
-                USER_NAME
-            </div>
-        </div>
-    )
+  return (
+    <nav>
+      <div className="navigation-logo">
+        <Link>
+          <img className="navigation-logo__img" src={logo} alt="Webdock logo" />
+        </Link>
+      </div>
+      <ul className="">
+        <li>
+          <Link>Roadmap</Link>
+        </li>
+        <li>
+          <Link>Feature Request</Link>
+        </li>
+        <li>
+          <Link>Profil</Link>
+        </li>
+        <li>
+          <div>USER_NAME</div>
+        </li>
+      </ul>
+    </nav>
+  );
 }
