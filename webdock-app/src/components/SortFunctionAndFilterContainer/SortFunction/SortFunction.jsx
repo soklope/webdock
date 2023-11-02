f2b84bd90aa974ed30f7c673419623c48c3b9096
alvihelene <95623140+alvihelene@users.noreply.github.com>
2023-11-02 22:44:51 +0100

    Changes to the buttons in the dropdown menu

--- webdock-app/src/components/SortFunctionAndFilterContainer/SortFunction/SortFunction.jsx
+++ webdock-app/src/components/SortFunctionAndFilterContainer/SortFunction/SortFunction.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react';
 import '../../SortFunctionAndFilterContainer/SortFunction/SortFunction.scss';
-import DropdownMenu from '../../SortFunctionAndFilterContainer/SortFunction/DropdownMenu/DropdownMenu.jsx';
 
 export default function SortFunction() {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
-  const [selectedSortOption, setSelectedSortOption] = useState(''); // To store the selected sort option
+  const [selectedSortOption, setSelectedSortOption] = useState('');
 
   const toggleDropdown = () => {
     setDropdownOpen(!isDropdownOpen);
@@ -12,8 +11,12 @@ export default function SortFunction() {
 
   const handleSortChange = (sortOption) => {
     setSelectedSortOption(sortOption);
-    setDropdownOpen(false); // Close the dropdown after clicking on an option
+    setDropdownOpen(false);
     // Implement your sorting logic here based on the selected sort option
+  };
+
+  const handleSortClick = (sortOption) => {
+    handleSortChange(sortOption);
   };
 
   return (
@@ -22,11 +25,18 @@ export default function SortFunction() {
         className={`sort-function-btn ${isDropdownOpen ? 'active' : ''}`}
         onClick={toggleDropdown}
       >
-        {selectedSortOption ? selectedSortOption : 'Sort'} {/* Update the button text */}
+        {selectedSortOption ? selectedSortOption : 'Sort'}
         <span className="sort-function-btn__icon"></span>
       </button>
-      {isDropdownOpen && <DropdownMenu onSortChange={handleSortChange} />}
-      {/* Display the selected sort option or perform sorting based on selectedSortOption */}
+      {isDropdownOpen && (
+        <div className="dropdown-menu">
+          <ul>
+            <li onClick={() => handleSortClick('Trending')}>Trending</li>
+            <li onClick={() => handleSortClick('Top')}>Top</li>
+            <li onClick={() => handleSortClick('New')}>New</li>
+          </ul>
+        </div>
+      )}
       {selectedSortOption && <p>Selected Sort: {selectedSortOption}</p>}
     </div>
   );
