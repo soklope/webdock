0a129696816af39b5b2b5fdf215de68d5e2009da
alvihelene <95623140+alvihelene@users.noreply.github.com>
2023-11-02 19:30:05 +0100

    Last fix on the sort filter button

--- webdock-app/src/components/SortFunctionAndFilterContainer/SortFunction/SortFunction.jsx
+++ webdock-app/src/components/SortFunctionAndFilterContainer/SortFunction/SortFunction.jsx
@@ -3,30 +3,31 @@ import '../../SortFunctionAndFilterContainer/SortFunction/SortFunction.scss';
 import DropdownMenu from '../../SortFunctionAndFilterContainer/SortFunction/DropdownMenu/DropdownMenu.jsx';
 
 export default function SortFunction() {
-    const [isDropdownOpen, setDropdownOpen] = useState(false);
-    const [selectedSortOption, setSelectedSortOption] = useState(''); // To store the selected sort option
-  
-    const toggleDropdown = () => {
-      setDropdownOpen(!isDropdownOpen);
-    };
-  
-    const handleSortChange = (sortOption) => {
-      setSelectedSortOption(sortOption); // Set the selected sort option as-is
-      // Implement your sorting logic here based on the selected sort option
-    };
-  
-    return (
-      <div className="sort-function">
-        <button
-          className={`sort-function-btn ${isDropdownOpen ? 'active' : ''}`}
-          onClick={toggleDropdown}
-        >
-          {selectedSortOption ? selectedSortOption : 'Sort'} {/* Update the button text */}
-          <span className="sort-function-btn__icon"></span>
-        </button>
-        {isDropdownOpen && <DropdownMenu onSortChange={handleSortChange} />}
-        {/* Display the selected sort option or perform sorting based on selectedSortOption */}
-        {selectedSortOption && <p>Selected Sort: {selectedSortOption}</p>}
-      </div>
-    );
-  }
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [selectedSortOption, setSelectedSortOption] = useState(''); // To store the selected sort option
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleSortChange = (sortOption) => {
+    setSelectedSortOption(sortOption);
+    setDropdownOpen(false); // Close the dropdown after clicking on an option
+    // Implement your sorting logic here based on the selected sort option
+  };
+
+  return (
+    <div className="sort-function">
+      <button
+        className={`sort-function-btn ${isDropdownOpen ? 'active' : ''}`}
+        onClick={toggleDropdown}
+      >
+        {selectedSortOption ? selectedSortOption : 'Sort'} {/* Update the button text */}
+        <span className="sort-function-btn__icon"></span>
+      </button>
+      {isDropdownOpen && <DropdownMenu onSortChange={handleSortChange} />}
+      {/* Display the selected sort option or perform sorting based on selectedSortOption */}
+      {selectedSortOption && <p>Selected Sort: {selectedSortOption}</p>}
+    </div>
+  );
+}
