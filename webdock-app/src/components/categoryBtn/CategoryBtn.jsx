fb47d2e8abc8b8d9e8f58078a184f95486848476
sophia nguyen <146834625+Sophiangu@users.noreply.github.com>
2023-11-02 14:17:48 +0100

    20% done - Fixing bug

---
+++ webdock-app/src/components/categoryBtn/CategoryBtn.jsx
@@ -0,0 +1,10 @@
+// import { useState} from "react";
+import "../categoryBtn/CategoryBtn.scss"
+
+export default function CategoryBtn(){
+    return <button className='category-btn-mypost-text'>
+      <p>My Post</p>
+    </button> 
+}
+
+
