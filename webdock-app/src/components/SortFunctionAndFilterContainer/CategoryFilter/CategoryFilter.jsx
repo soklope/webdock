b17800eb3210c9ee144a69bd6dcb6f6619b636b3
alvihelene <95623140+alvihelene@users.noreply.github.com>
2023-11-03 21:06:16 +0100

    dummeBs.js array updated with accurate categories

---
+++ webdock-app/src/components/SortFunctionAndFilterContainer/CategoryFilter/CategoryFilter.jsx
@@ -0,0 +1,13 @@
+
+// All Categories
+// Dashboard Features
+// Documentation
+// Billing Features
+// Networking
+// Hardware and products
+// Perfect Ser Stacks
+// Mobile App
+// Webdock API
+// Competition
+
+// Dashboard Features, Documentation, Billing Features, Networking, Hardware and products, Perfect Ser Stacks, Mobile App, Webdock API, Competition
