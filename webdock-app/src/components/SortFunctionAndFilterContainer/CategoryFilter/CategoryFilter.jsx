3546ba7afc4824f27bed3ca1581bdf543bafb0fe
alvihelene <95623140+alvihelene@users.noreply.github.com>
2023-11-03 22:13:21 +0100

    CategoryFilter.jsx functional. Needs proper styling.

--- webdock-app/src/components/SortFunctionAndFilterContainer/CategoryFilter/CategoryFilter.jsx
+++ webdock-app/src/components/SortFunctionAndFilterContainer/CategoryFilter/CategoryFilter.jsx
@@ -1,13 +1,60 @@
+import { useState } from 'react';
+import '../CategoryFilter/CategoryFilter.scss';
+import { plannedArrayDb, inProgressArrayDb, completeArrayDb } from '../../../dummyDb';
 
-// All Categories
-// Dashboard Features
-// Documentation
-// Billing Features
-// Networking
-// Hardware and products
-// Perfect Ser Stacks
-// Mobile App
-// Webdock API
-// Competition
+export default function CategoryFilter() {
+  const [selectedCategory, setSelectedCategory] = useState('All Categories');
+  const [showDropdown, setShowDropdown] = useState(false);
 
-// Dashboard Features, Documentation, Billing Features, Networking, Hardware and products, Perfect Ser Stacks, Mobile App, Webdock API, Competition
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  const filterPosts = (data) => {
+    if (selectedCategory === 'All Categories') {
+      return data; // Return all posts if no category is selected.
+    }
+
+    return data.filter(post => post.category === selectedCategory);
+  };
+
+  return (
+    <div className="category-filter">
+      <button onClick={toggleDropdown} className="category-filter-btn">
+        {selectedCategory}
+        <span className="category-filter-btn__icon"></span>
+      </button>
+      {showDropdown && (
+        <div className="category-dropdown">
+          <select onChange={(e) => setSelectedCategory(e.target.value)}>
+            <option value="All Categories">All Categories</option>
+            <option value="Dashboard Features">Dashboard Features</option>
+            <option value="Documentation">Documentation</option>
+            <option value="Billing Features">Billing Features</option>
+            <option value="Networking">Networking</option>
+            <option value="Hardware and Products">Hardware and Products</option>
+            <option value="Perfect Server Stacks">Perfect Server Stacks</option>
+            <option value="Mobile App">Mobile App</option>
+            <option value="Webdock API">Webdock API</option>
+            <option value="Competition">Competition</option>
+          </select>
+        </div>
+      )}
+      <div className="filtered-posts">
+        {filterPosts([...plannedArrayDb, ...inProgressArrayDb, ...completeArrayDb]).map(post => (
+          <div key={post.title}>
+            <h3>{post.title}</h3>
+            <p>{post.description}</p>
+            <p>Category: {post.category}</p>
+            {/* Add more post information here */}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+
+
+
+
--- webdock-app/src/components/SortFunctionAndFilterContainer/SortFunctionFilterContainer.jsx
+++ webdock-app/src/components/SortFunctionAndFilterContainer/SortFunctionFilterContainer.jsx
@@ -1,11 +1,12 @@
 import '../SortFunctionAndFilterContainer/SortFunctionFilterContainer.scss';
+import CategoryFilter from './CategoryFilter/CategoryFilter';
 import SortFunction from './SortFunction/SortFunction';'./SortFunction/SortFunction.jsx'
 
 export default function SortFunctionAndFilterContainer() {
     return (
         <div className="sort-function-and-filter-container">
             <SortFunction/>
-            <SortFunction />
+            <CategoryFilter />
             <SortFunction />   
         </div>
     )
