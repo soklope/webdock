d368030a94a5fa45e534a27a5c44cf4a7c572aa0
alvihelene <95623140+alvihelene@users.noreply.github.com>
2023-11-03 22:53:44 +0100

    Revert "CategoryFilter.jsx functional. Needs proper styling."
    This reverts commit 3546ba7afc4824f27bed3ca1581bdf543bafb0fe.

--- webdock-app/src/components/SortFunctionAndFilterContainer/SortFunctionFilterContainer.jsx
+++ webdock-app/src/components/SortFunctionAndFilterContainer/SortFunctionFilterContainer.jsx
@@ -1,12 +1,11 @@
 import '../SortFunctionAndFilterContainer/SortFunctionFilterContainer.scss';
-import CategoryFilter from './CategoryFilter/CategoryFilter';
 import SortFunction from './SortFunction/SortFunction';'./SortFunction/SortFunction.jsx'
 
 export default function SortFunctionAndFilterContainer() {
     return (
         <div className="sort-function-and-filter-container">
             <SortFunction/>
-            <CategoryFilter />
+            <SortFunction />
             <SortFunction />   
         </div>
     )
