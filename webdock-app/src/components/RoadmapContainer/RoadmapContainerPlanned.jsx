a870646d7db7a6063ef2518a9418a69a19f67082
soklope <95622881+soklope@users.noreply.github.com>
2023-11-06 13:28:15 +0100

    Update RoadmapContainerPlanned.jsx
    Optimized rendering of container

--- webdock-app/src/components/RoadmapContainer/RoadmapContainerPlanned.jsx
+++ webdock-app/src/components/RoadmapContainer/RoadmapContainerPlanned.jsx
@@ -1,76 +1,49 @@
 import { useState, useEffect } from "react";
 import "./RoadmapContainer.scss";
-
 import { plannedArrayDb } from "../../dummyDb";
 import RoadmapChildren from "../RoadmapChildren/RoadmapChildren";
 
-
 export default function RoadmapContainerPlanned() {
-
-  const [containerIsOpen, setContainerIsOpen] = useState(false)
-  const [postCount, setPostCount] = useState(0)
+  const [containerIsOpen, setContainerIsOpen] = useState(false);
+  const [postCount, setPostCount] = useState(0);
 
   useEffect(() => {
-    setPostCount(plannedArrayDb.length)
-  }, [])
+    setPostCount(plannedArrayDb.length);
+  }, []);
 
   const openContainer = () => {
-    setContainerIsOpen(!containerIsOpen)
-  }
+    setContainerIsOpen(!containerIsOpen);
+  };
+
+  const renderRoadmapChildren = () => {
+    return plannedArrayDb.map((post, index) => (
+      <div key={index} className="roadmap-child-container">
+        <RoadmapChildren
+          title={post.title}
+          category={post.category}
+          status={post.status}
+          numberOfComments={post.numberOfComments}
+          totalUpvotes={post.numberOfUpvotes}
+          statusColor={"tag-planned-color"}
+        />
+      </div>
+    ));
+  };
 
   return (
-
-    <>
-      <div className="roadmap-container">
-        <div className="roadmap-container__tooltip" onClick={openContainer}>
-          <p>Planned</p>
-          <span className={`roadmap-container__dropdown-icon ${containerIsOpen ? "rotate" : ""}`}></span>
-          <p className="roadmap-container__postcount">{postCount}</p>
-          <div className={`roadmap-container__color-indicator planned-color`}/>
-        </div>
-  
-        { containerIsOpen  && 
-          
-          <div className="roadmap-container__dropdown">
-            <>
-              {plannedArrayDb.map((post, index) => (
-                  <div key={index} className="roadmap-child-container">
-                  <RoadmapChildren 
-                    title={post.title}
-                    category={post.category}
-                    status={post.status}
-                    numberOfComments={post.numberOfComments}
-                    totalUpvotes={post.numberOfUpvotes}
-                    statusColor={"tag-planned-color"}
-                  />
-                </div>
-              ))}
-            </>
-          </div>
-
-        }
-
-        <div className="roadmap-container__children">
-          <>
-              {plannedArrayDb.map((post, index) => (
-                  <div key={index} className="roadmap-child-container">
-                    <RoadmapChildren 
-                      title={post.title}
-                      category={post.category}
-                      status={post.status}
-                      numberOfComments={post.numberOfComments}
-                      totalUpvotes={post.numberOfUpvotes}
-                      statusColor={"tag-planned-color"}
-                    />
-                  </div>
-              ))}
-          </>
-        </div>
+    <div className="roadmap-container">
+      <div className="roadmap-container__tooltip" onClick={openContainer}>
+        <p>Planned</p>
+        <span className={`roadmap-container__dropdown-icon ${containerIsOpen ? "rotate" : ""}`}></span>
+        <p className="roadmap-container__postcount">{postCount}</p>
+        <div className="roadmap-container__color-indicator planned-color" />
       </div>
-    </>
-  )
-}
 
-// Todo fter code review 
-// Clean up react fragments
-// More transparency either in comments or better classes (cant see what is mobile and what is desktop)
+      {containerIsOpen && (
+        <div className="roadmap-container__dropdown">{renderRoadmapChildren()}</div>
+      )}
+
+      <div className="roadmap-container__children">{renderRoadmapChildren()}</div>
+    </div>
+  );
+}
