74620e146b3a867f54c145606803905cb8ea7ee0
alvihelene <95623140+alvihelene@users.noreply.github.com>
2023-11-06 11:30:54 +0100

    Updated sort and filter, moved to own route
    also updated background
    
    Co-Authored-By: KasperPtak <60694854+KasperPtak@users.noreply.github.com>
    Co-Authored-By: dittekoustrup <115533174+dittekoustrup@users.noreply.github.com>
    Co-Authored-By: soklope <95622881+soklope@users.noreply.github.com>

--- webdock-app/src/router/router.jsx
+++ webdock-app/src/router/router.jsx
@@ -5,6 +5,7 @@ import Navigation from '../layout/navigation/Navigation';
 import RoadmapView from '../views/RoadmapView';
 import ListView from '../views/ListView';
 import SinglePostView from '../views/SinglePostView';
+import SortFunctionAndFilterContainer from '../components/SortFunctionAndFilterContainer/SortFunctionFilterContainer';
 
 const Router = () => {
   return (
@@ -14,6 +15,7 @@ const Router = () => {
         <Route exact path="/" element={<RoadmapView />} />
         <Route path="/listview" element={<ListView />} />
         <Route path="/post" element={<SinglePostView />} /> 
+        <Route path="/sort" element={<SortFunctionAndFilterContainer />} /> 
         {/* Add more routes here as needed */}
       </Routes>
     </BrowserRouter>
