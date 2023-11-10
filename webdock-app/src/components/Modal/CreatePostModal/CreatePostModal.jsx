769f381cd1d65d06b1f038bef113b3d916ffee84
dittekoustrup <dittekoustrup@outlook.com>
2023-11-10 09:07:45 +0100

    finished semi visual look
    Added all the components for the create post modal, and did the visuals, but still need cleanup.

--- webdock-app/src/components/Modal/CreatePostModal/CreatePostModal.jsx
+++ webdock-app/src/components/Modal/CreatePostModal/CreatePostModal.jsx
@@ -1,6 +1,8 @@
 import ModalBackground from "../ModalBackground";
 import SelectCategory from "../SelectCategory/SelectCategory";
 import './CreatePostModal.scss'
+import '../../CreatePostBtn/CreatePostBtn'
+import CreatePostBtn from "../../CreatePostBtn/CreatePostBtn";
 
 export default function CreatePostModal() {
     return(
@@ -8,14 +10,29 @@ export default function CreatePostModal() {
             <ModalBackground />
         <div className="container">
             <div className="create-post-modal-container">
-                <div className="category">
+                {/* <div className="category">
                     <p className="titles">Category</p>
-                     <SelectCategory />
-                </div>
-                <div className="titel-input">
-                    <p className="titles">Title</p>
-                    <input name="Titel" placeholder="Short, discriptive title" />
-                </div>
+                        <SelectCategory />
+                </div> */}
+
+                <form>
+                    <label>Category</label>
+                    <div className="category">
+                    <SelectCategory />
+                    </div>
+                    <label>Titel</label><br />
+                    <input 
+                    type="text" 
+                    placeholder="Short, discriptive title"
+                    />
+                    <label>Details</label><br />
+                    <textarea
+                        placeholder="Any additional detail...."
+                    ></textarea> 
+                    <button><img src="../../../content/gfx/Icons/files.svg" alt="Clip icon" /></button>
+                    < CreatePostBtn />
+                </form>
+                
             </div>
         </div>
         </>
