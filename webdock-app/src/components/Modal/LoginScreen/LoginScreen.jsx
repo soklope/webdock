7e39dc31a195510a800168ada98de8511aef0bad
soklope <95622881+soklope@users.noreply.github.com>
2023-11-20 23:18:21 +0100

    Fixed hover effects and blur on login

--- webdock-app/src/components/Modal/LoginScreen/LoginScreen.jsx
+++ webdock-app/src/components/Modal/LoginScreen/LoginScreen.jsx
@@ -1,18 +1,31 @@
 import './LoginScreen.scss';
+import userStore from '../../../stores/loginStore';
 
 export default function LoginScreen() {
+
+  const { loginAsUser } = userStore()
+
+  const handleLoginClick = () => {
+    loginAsUser();
+
+    const loggedInUser = localStorage.getItem('user');
+
+    if (loggedInUser) {
+      window.location.href = "/"
+    }
+  };
 
   return (
     <div className='login-page wrap'>
       <div className='login-page__container'>
         <span className='login-page__logo'></span>
         <input className='login-page__input' type="text" placeholder='example@gmail.com'/>
-        <input className='login-page__input' type="text" placeholder='example@gmail.com'/>
+        <input className='login-page__input' type="password" placeholder='password'/>
         <div className='login-page__checkbox'>
           <input type="checkbox" />
           <p>Stay signed in for two weeks</p>
         </div>
-        <button className='login-page__login-button'>LOG IN</button>
+        <button onClick={handleLoginClick} className='login-page__login-button'>LOG IN</button>
         <hr />
         <button className='login-page__google-button'>Sign in with Google</button>
         <button className='login-page__github-button'>Sign in with Github</button>
@@ -22,6 +35,7 @@ export default function LoginScreen() {
       </div>
 
       <div className='login-page__splash-art'>
+        <div className='login-page__splash-art__blur'/>
         <p className='login-page__splash-art__headline'>Skip the email hassle <br></br> join Webdock's swift forum!</p>
         <p className='login-page__splash-art__subline'>Where communication meets simplicity in the cloud</p>
         <span className='login-page__logo'></span>
