9bb0c597fdfffb5fe762e6cb5d3f536f61514eb0
soklope <95622881+soklope@users.noreply.github.com>
2023-11-20 23:27:32 +0100

    Update SinglePostHeading.jsx

--- webdock-app/src/components/SinglePost/SinglePostHeading/SinglePostHeading.jsx
+++ webdock-app/src/components/SinglePost/SinglePostHeading/SinglePostHeading.jsx
@@ -4,8 +4,26 @@ import UpvoteBtn from "../../UpvoteBtn/UpvoteBtn";
 import Username from "../../Username/Username";
 
 import "./SinglePostHeading.scss";
+import LabelTag from "../../LabelTag/LabelTag";
 
 export default function SinglePostHeading({ postTitle, postStatus, postDate, postCategory, postAuthor, postUpvotes }) {
+
+    function getColorTagFromStatus(status) {
+        switch (status) {
+            case 'My Post':
+                return 'tag-my-post-color';
+            case 'Review':
+                return 'tag-review-color';
+            case 'Planned':
+                return 'tag-planned-color';
+            case 'In Progress':
+                return 'tag-in-progress-color';
+            case 'Complete':
+                return 'tag-complete-color';
+            default:
+                return '';
+        }
+    }
 
 	return (
 		<>
@@ -14,7 +32,10 @@ export default function SinglePostHeading({ postTitle, postStatus, postDate, pos
 					<h1 className="single-post-heading__title">{postTitle}</h1>
 
 					<div className="single-post-heading__meta-container">
-						<div>STATUS: {postStatus} </div>
+						<LabelTag 
+							LabelStatus={postStatus}
+							LabelColor={getColorTagFromStatus(postStatus)}
+						/>
 						<div className="single-post-heading__desktop">
 							<div>Category: {postCategory}</div>
 							<span>•</span>
