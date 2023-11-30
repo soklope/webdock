2dc00bd3406265e3d777a5dc4be86921ab459169
dittekoustrup <dittekoustrup@outlook.com>
2023-11-30 11:18:35 +0100

    updated Verify to fit new endpoint
    Co-Authored-By: KasperPtak <60694854+KasperPtak@users.noreply.github.com>
    Co-Authored-By: Sophiangu <146834625+Sophiangu@users.noreply.github.com>
    Co-Authored-By: alvihelene <95623140+alvihelene@users.noreply.github.com>
    Co-Authored-By: soklope <95622881+soklope@users.noreply.github.com>

--- webdock-app/src/services/ssoService.js
+++ webdock-app/src/services/ssoService.js
@@ -3,7 +3,7 @@ export const fetchData = async () => {
         const urlParams = new URLSearchParams(window.location.search)
         const ssoToken = urlParams.get('ssoToken')
 
-        const response = await fetch("http://localhost:3000/verify", {
+        const response = await fetch("http://localhost:8080/verify", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
