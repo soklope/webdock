500d09b1d4c1b4329c13cef40a55c5f39bb5ee32
alvihelene <95623140+alvihelene@users.noreply.github.com>
2023-12-05 15:59:00 +0100

    Basic POST functionality works now
    Needs to be able to check duplicate handling and error handling.
    
    Co-Authored-By: KasperPtak <60694854+KasperPtak@users.noreply.github.com>
    Co-Authored-By: Sophiangu <146834625+Sophiangu@users.noreply.github.com>
    Co-Authored-By: dittekoustrup <115533174+dittekoustrup@users.noreply.github.com>

--- webdock-app/src/components/Modal/LoginScreen/LoginScreen.jsx
+++ webdock-app/src/components/Modal/LoginScreen/LoginScreen.jsx
@@ -5,18 +5,24 @@ import { redirectToWebdock } from '../../../services/ssoService';
 export default function LoginScreen() {
 
   // const { loginAsUser, loginAsAdmin, loginWithSso } = userStore()
-
-
-const handleLogin = async () => {
-  const response = await fetch ('http://localhost:8080/api/v1/handlelogin', {
-    method:'POST',
-    body: JSON.stringify({
+const alvi = {
       'avatarURL': "",
       'email': "ahmo38299@edu.ucl.dk",
       'id': 22654,
       'name': "Alvi Møller"
-    })
+}
+
+const handleLogin = async () => {
+  console.log(alvi);
+  const response = await fetch ('http://localhost:8080/api/v1/handlelogin', {
+    method:'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(alvi)
   })
+  console.log(response);
+  
   const result = response.json();
   console.log(result)
 }
