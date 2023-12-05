5f11a075150f9bf20d5b3af71ce2b4ddf9e57bd7
alvihelene <95623140+alvihelene@users.noreply.github.com>
2023-12-05 15:28:03 +0100

    Connection to the server is made
    But it need to post the data. Right now it only makes createdAt and a new id.

--- webdock-app/src/components/Modal/LoginScreen/LoginScreen.jsx
+++ webdock-app/src/components/Modal/LoginScreen/LoginScreen.jsx
@@ -1,10 +1,25 @@
 import './LoginScreen.scss';
-import userStore from '../../../stores/loginStore';
+// import userStore from '../../../stores/loginStore';
 import { redirectToWebdock } from '../../../services/ssoService';
 
 export default function LoginScreen() {
 
   // const { loginAsUser, loginAsAdmin, loginWithSso } = userStore()
+
+
+const handleLogin = async () => {
+  const response = await fetch ('http://localhost:8080/api/v1/handlelogin', {
+    method:'POST',
+    body: JSON.stringify({
+      'avatarURL': "",
+      'email': "ahmo38299@edu.ucl.dk",
+      'id': 22654,
+      'name': "Alvi Møller"
+    })
+  })
+  const result = response.json();
+  console.log(result)
+}
 
   return (
     <div className='login-page wrap'>
@@ -13,12 +28,15 @@ export default function LoginScreen() {
         <button onClick={redirectToWebdock} className='login-page__google-button'>Sign in with Webdock</button>
         <p>Don't have an account yet?</p>
         <p className='login-page__link-text'>sign up here!</p>
+      <button onClick={ handleLogin }> Hej </button>
       </div>
 
       <div className='login-page__splash-art'>
         <div className='login-page__splash-art__blur'/>
         <p className='login-page__splash-art__headline'>Skip the email hassle <br></br> join Webdock's swift forum!</p>
         <p className='login-page__splash-art__subline'>Where communication meets simplicity in the cloud</p>
+      
+
       </div>
     </div>
   );
