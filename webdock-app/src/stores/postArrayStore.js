9b51242eee91a947a6432b0ef69a4a7c63b250f1
soklope <skp2104@hotmail.com>
2023-12-05 14:19:21 +0100

    Fixed filter bug

--- webdock-app/src/stores/postArrayStore.js
+++ webdock-app/src/stores/postArrayStore.js
@@ -32,27 +32,27 @@ const usePostArrayStore = create((set) => ({
     filterAllPosts: () => {
         set((state) => {
             let filteredPosts = [...state.sustainAllPosts];
-    
+
             switch (state.sort) {
                 case 'Trending':
                     filteredPosts.sort((a, b) => b.Comments.length - a.Comments.length);
                     break;
-    
+
                 case 'Top':
                     filteredPosts.sort((a, b) => b.upvotes - a.upvotes);
                     break;
-    
+
                 case 'New':
                     filteredPosts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
                     break;
             }
-    
+
             filteredPosts = filteredPosts.filter((post) =>
-                (state.category === 'All Categories' || post.Category.category === state.category) &&
+                (state.category === 'All Categories' || (post.Category && post.Category.category === state.category)) &&
                 (state.searchValue === '' || post.title.toLowerCase().includes(state.searchValue.toLowerCase())) &&
-                (state.statusFilter.length === 0 || state.statusFilter.includes(post.Status.status))
+                (state.statusFilter.length === 0 || (post.Status && state.statusFilter.includes(post.Status.status)))
             );
-    
+
             return { allPosts: filteredPosts };
         });
     },
