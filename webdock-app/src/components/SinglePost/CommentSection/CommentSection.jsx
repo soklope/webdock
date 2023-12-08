6dffef623f3a6ffd3bbab4db684554aa3a71cbca
alvihelene <95623140+alvihelene@users.noreply.github.com>
2023-12-08 22:14:16 +0100

    Styling done for post-comments inputfield.
    Next is connecting it to the database. The comment needs to be posted with comment_id, post_id and user_id to the database.

--- webdock-app/src/components/SinglePost/CommentSection/CommentSection.jsx
+++ webdock-app/src/components/SinglePost/CommentSection/CommentSection.jsx
@@ -7,6 +7,25 @@ import SingleComment from "./Comment/SingleComment.jsx";
 export default function CommentSection({ comments }) {
 	return (
 		<div className="comment-section-container">
+			<div className="comment-post-container">
+				
+				<textarea 
+					className="comment-post"
+					type="text"
+					placeholder="Leave a comment">
+				</textarea>
+
+				<button className="submit-comment">
+					SUBMIT
+				</button>
+			</div>
+
+			<div className="comment-section-container">
+				<div className="log-in-to-comment">
+					<p>Log in to leave a comment</p>
+				</div>
+			</div>
+
 			{comments.map((comment) => (
 				<SingleComment key={comment.id} {...comment} />
 			))}
