8742e866dbc1d3dabdb4e8740b47e503cad5f70b
alvihelene <95623140+alvihelene@users.noreply.github.com>
2023-12-10 16:50:45 +0100

    Comment content is now posted to the database. Still miss the userID

--- webdock-app/src/components/SinglePost/CommentSection/CommentSection.jsx
+++ webdock-app/src/components/SinglePost/CommentSection/CommentSection.jsx
@@ -1,10 +1,40 @@
-import { React } from "react";
-
+import { useState } from "react";
 import "./CommentSection.scss";
-
 import SingleComment from "./Comment/SingleComment.jsx";
 
 export default function CommentSection({ comments }) {
+const [commentData, setCommentData] = useState({
+    content: "detail",
+    user_id: 0,
+  });
+
+const handleSubmit = async () => {
+    try {
+	const response = await fetch("http://localhost:8080/api/v1/createcomment", {
+		method: "POST",
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			content: commentData.content,
+			user_id: commentData.user_id,
+		}),
+	});
+
+      if (response.ok) {
+        const result = await response.json();
+        console.log("Response:", result);
+
+			//needs to post the new comment somehow
+      } else {
+        console.error("Error:", response.status);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+
 	return (
 		<div className="comment-section-container">
 			<div className="comment-post-container">
@@ -12,19 +42,27 @@ export default function CommentSection({ comments }) {
 				<textarea 
 					className="comment-post"
 					type="text"
-					placeholder="Leave a comment">
-				</textarea>
+					placeholder="Leave a comment"
+					onKeyUp={(event) =>
+                    setCommentData((prevData) => ({
+                      ...prevData,
+                      content: event.target.value,
+                    }))
+                  }
+				></textarea>
 
-				<button className="submit-comment">
+				<button 
+				className="submit-comment"
+				onClick={handleSubmit}>
 					SUBMIT
 				</button>
 			</div>
 
-			<div className="comment-section-container">
-				<div className="log-in-to-comment">
+
+				{/* <div className="log-in-to-comment">
 					<p>Log in to leave a comment</p>
-				</div>
-			</div>
+				</div> */}
+
 
 			{comments.map((comment) => (
 				<SingleComment key={comment.id} {...comment} />
