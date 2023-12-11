7ec20b9cda5ccd9fa11510bdd69119e65c3b8f72
alvihelene <95623140+alvihelene@users.noreply.github.com>
2023-12-11 10:42:43 +0100

    Date now formatting to day-month-year instead of month-day-year

--- webdock-app/src/helper/formatDate.js
+++ webdock-app/src/helper/formatDate.js
@@ -2,7 +2,7 @@
 
 const formatCustomDate = (inputDate) => {
     const options = { month: '2-digit', day: '2-digit', year: 'numeric' };
-    const formattedDate = inputDate.toLocaleDateString('en-US', options);
+    const formattedDate = inputDate.toLocaleDateString('en-GB', options);
     const customFormat = formattedDate.replace(',', ' -');
 
     return customFormat;
