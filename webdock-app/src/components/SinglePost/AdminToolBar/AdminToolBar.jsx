bc6b61ce0a02ec3e0fc14f1c46c59d0dbd653005
KasperPtak <kasperptak2@gmail.com>
2023-12-12 11:34:24 +0100

    Setting up fetch for handling merges
    params are hardcoded for now
    
    Co-Authored-By: Sophiangu <146834625+Sophiangu@users.noreply.github.com>
    Co-Authored-By: soklope <95622881+soklope@users.noreply.github.com>
    Co-Authored-By: dittekoustrup <115533174+dittekoustrup@users.noreply.github.com>
    Co-Authored-By: alvihelene <95623140+alvihelene@users.noreply.github.com>

--- webdock-app/src/components/SinglePost/AdminToolBar/AdminToolBar.jsx
+++ webdock-app/src/components/SinglePost/AdminToolBar/AdminToolBar.jsx
@@ -24,7 +24,6 @@ export default function AdminToolBar( {itemId} ) {
     };
 
   const handleChangeStatus = async (newStatus) => {
-
     try {
         const response = await fetch(`http://localhost:8080/api/v1/changePostStatus/${itemId}/status/${newStatus}`, {
         method: 'PATCH', // You may need to change this to 'PUT' or 'PATCH' depending on your server implementation
@@ -38,6 +37,25 @@ export default function AdminToolBar( {itemId} ) {
         console.log('Post status updated successfully', data);
         } else {
         console.error('Failed to update post status');
+        }
+    } catch (error) {
+        console.error('Error updating post status:', error);
+    }
+  };
+  const handleMerge = async (postId, newParent) => {
+    try {
+        const response = await fetch(`http://localhost:8080/api/v1/mergepost/${postId}/newparent/${newParent}`, {
+        method: 'PATCH', // You may need to change this to 'PUT' or 'PATCH' depending on your server implementation
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        });
+
+        if (response.ok) {
+        const data = await response.json();
+        console.log('Post merged successfully', data);
+        } else {
+        console.error('Failed to merge posts');
         }
     } catch (error) {
         console.error('Error updating post status:', error);
@@ -61,7 +79,7 @@ export default function AdminToolBar( {itemId} ) {
               }
           </div>
 
-          <div className="admin-toolbar-container__tool">
+          <div className="admin-toolbar-container__tool" onClick={() => handleMerge(2, 1)}>
               <p>Merge</p>
               <span className="admin-toolbar-container__tool__merge" />
           </div>
