73202e10855845d7d337c31fce179b64d46bf8e9
soklope <skp2104@hotmail.com>
2023-12-12 11:33:08 +0100

    Update checkAdmin.js
    Co-Authored-By: KasperPtak <60694854+KasperPtak@users.noreply.github.com>
    Co-Authored-By: Sophiangu <146834625+Sophiangu@users.noreply.github.com>
    Co-Authored-By: dittekoustrup <115533174+dittekoustrup@users.noreply.github.com>
    Co-Authored-By: alvihelene <95623140+alvihelene@users.noreply.github.com>

--- webdock-app/src/helper/checkAdmin.js
+++ webdock-app/src/helper/checkAdmin.js
@@ -1,6 +1,6 @@
 export const checkAdmin = (userEmail) => {
   // if (userEmail.endsWith("@webdock.io" || "@edu.ucl.dk")) {
-  if (userEmail.endsWith("@edu.ucl.dk")) {
+  if (userEmail && userEmail.endsWith("@edu.ucl.dk")) {
     return true;
   } else {
     return false;
