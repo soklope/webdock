2898cc75af31db6d295e70f1aa6aa2ca6e9f1382
dittekoustrup <dittekoustrup@outlook.com>
2023-12-14 17:20:36 +0100

    Updated toggle switch btn
    btn can no longer be pressed when active, and they only show their related views
    
    Co-Authored-By: KasperPtak <60694854+KasperPtak@users.noreply.github.com>

--- webdock-app/src/components/ViewToggleSwitchContainer/ViewToggleSwitch/ViewToggleSwitch.jsx
+++ webdock-app/src/components/ViewToggleSwitchContainer/ViewToggleSwitch/ViewToggleSwitch.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import "../ViewToggleSwitch/ViewToggleSwitch.scss";
 
 // This component represents a toggle switch that takes 2 props.
-export default function ViewToggleSwitch({ switchIcon, onToggle }) {
+export default function ViewToggleSwitch({ switchIcon, onToggle, isRoadmap }) {
   // Renders a clickable toggle switch element.
   return (
     <button className="view-toggle-switch ">
-      <span className={switchIcon} onClick={onToggle}></span>
+      <span className={switchIcon} onClick={() => onToggle(isRoadmap)}></span>
     </button>
   );
 }
--- webdock-app/src/components/ViewToggleSwitchContainer/ViewToggleSwitchContainer.jsx
+++ webdock-app/src/components/ViewToggleSwitchContainer/ViewToggleSwitchContainer.jsx
@@ -6,31 +6,32 @@ import useRoadmapStore from "../../stores/viewStore";
 // Component managing toggle switches between Roadmap and List views.
 export default function ViewToggleSwitchContainer() {
   const { toggleRoadmapView } = useRoadmapStore();
-  const [isRoadmapView, setIsRoadmapView] = useState(true); 
-  //const [displayText, setDisplayText] = useState("Roadmap View"); 
+  const [isRoadmapView, setIsRoadmapView] = useState(true);
 
   // Function to toggle between Roadmap and List views
-  const toggleView = () => {
-    setIsRoadmapView(!isRoadmapView);
-    toggleRoadmapView()
-    //setDisplayText(isRoadmapView ? "List View" : "Roadmap View");
+  const toggleView = (isRoadmap) => {
+    if (isRoadmap != isRoadmapView) {
+      setIsRoadmapView(!isRoadmapView);
+      toggleRoadmapView()
+    }
   };
 
   // Renders toggle switches with dynamic class names based on current view
   return (
-    //<p>{displayText}</p> below <div className="toggle-switches-container">
     <div className="toggle-switches-container">
       <ViewToggleSwitch
         switchIcon={
           `view-toggle-switch__roadmap${isRoadmapView ? "--active" : ""
-        }`}
+          }`}
         onToggle={toggleView}
+        isRoadmap={true}
       />
       <ViewToggleSwitch
         switchIcon={
           `view-toggle-switch__list${isRoadmapView ? "" : "--active"
-        }`}
+          }`}
         onToggle={toggleView}
+        isRoadmap={false}
       />
     </div>
   );
