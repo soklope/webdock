4c44dcbe339fe291ec3d3435589e037217aece5f
dittekoustrup <dittekoustrup@outlook.com>
2023-12-14 14:27:58 +0100

    Log in to create a post
    When you are logged out, you now get an alert to log in to create a new post
    
    Co-Authored-By: soklope <95622881+soklope@users.noreply.github.com>

--- webdock-app/src/components/CreatePostBtn/CreatePostBtn.jsx
+++ webdock-app/src/components/CreatePostBtn/CreatePostBtn.jsx
@@ -2,14 +2,15 @@ import './CreatePostBtn.scss'
 import useModalStore from '../../stores/modalStore';
 
 export default function CreatePostBtn() {
-
+  const user = localStorage.getItem('user');
   const { toggleModal } = useModalStore();
+  const userIsNotLoggedIn = () => {
+    alert('Log in to create a post')
+  }
 
   return (
-    <button className="create-post-btn" onClick={toggleModal}>
-      <span className='create-post-btn__icon'>
-        {/* You can add an icon here */}
-      </span>
+    <button className="create-post-btn" onClick={user ? toggleModal : userIsNotLoggedIn}>
+      <span className='create-post-btn__icon' />
       <p>CREATE POST</p>
     </button>
   );
