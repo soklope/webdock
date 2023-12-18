13eb3a491debc1963212c53997832bfa77179cc9
KasperPtak <kasperptak2@gmail.com>
2023-12-18 13:23:32 +0100

    formatting

--- webdock-app/src/components/SinglePost/SinglePostContent/SinglePostContent.jsx
+++ webdock-app/src/components/SinglePost/SinglePostContent/SinglePostContent.jsx
@@ -3,11 +3,11 @@ import React from "react";
 import "./SinglePostContent.scss";
 
 export default function SinglePostContent({ postContent }) {
-  
+  const formattedContent = postContent.replace(/\n/g, '<br>');
   return (
     <div>
       {/* important when using dangerouslySetInnerHTML, to sanitize data in database. It opens xss vulnerabilities */}
-      <p dangerouslySetInnerHTML={{ __html: postContent }}></p> 
+      <p dangerouslySetInnerHTML={{ __html: formattedContent }}></p> 
       
       {/* <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, cum iste?
