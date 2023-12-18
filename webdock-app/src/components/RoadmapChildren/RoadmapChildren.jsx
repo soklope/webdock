7574c504cfcc3690480f0dfb44978cb406eff707
KasperPtak <kasperptak2@gmail.com>
2023-12-18 14:23:19 +0100

    checking for myownstatus

--- webdock-app/src/components/RoadmapChildren/RoadmapChildren.jsx
+++ webdock-app/src/components/RoadmapChildren/RoadmapChildren.jsx
@@ -8,9 +8,10 @@ export default function RoadmapChildren({
   category,
   numberOfComments,
   totalUpvotes,
-  id
+  id,
+  myOwnStatus
 }) {
-
+console.log(myOwnStatus)
   return (
     <>
       <UpvoteBtn numberOfUpvotes={totalUpvotes} postId={id} />
@@ -20,7 +21,7 @@ export default function RoadmapChildren({
         className="roadmap-child-container__content"
       >
         <div className="roadmap-child-container__title-category-tag">
-          <p className="roadmap-child-container__title">{title} <span>{ myOwnStatus && <p className={`listview-child-container__tag tag-my-post-color`}>My Post</p>}</span> </p>
+          <p className="roadmap-child-container__title">{title}</p>
           <p className="roadmap-child-container__category">{category}</p>
           
         </div>
