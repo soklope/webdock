66dbb020b3197daf2d91e156bba7d6e91188de0a
KasperPtak <kasperptak2@gmail.com>
2023-12-18 16:01:31 +0100

    adding my posts to roadmapview

--- webdock-app/src/components/RoadmapChildren/RoadmapChildren.jsx
+++ webdock-app/src/components/RoadmapChildren/RoadmapChildren.jsx
@@ -1,32 +1,32 @@
 import "./RoadmapChildren.scss";
 import UpvoteBtn from "../UpvoteBtn/UpvoteBtn";
 
-import { Link } from 'react-router-dom';
- 
+import { Link } from "react-router-dom";
+
 export default function RoadmapChildren({
   title,
   category,
   numberOfComments,
   totalUpvotes,
   id,
-  myOwnStatus
+  myOwnStatus,
 }) {
-console.log(myOwnStatus, id)
   return (
     <>
       <UpvoteBtn numberOfUpvotes={totalUpvotes} postId={id} />
 
-      <Link
-        to={`/posts/${id}`}
-        className="roadmap-child-container__content"
-      >
+      <Link to={`/posts/${id}`} className="roadmap-child-container__content">
         <div className="roadmap-child-container__title-category-tag">
-          <p className="roadmap-child-container__title">
+          <p
+            className={
+              myOwnStatus
+                ? "roadmap-child-container__title roadmap-child-container__title--my-post"
+                : "roadmap-child-container__title"
+            }
+          >
             {title}
-            { myOwnStatus && <p className={`listview-child-container__tag tag-my-post-color`}>My Post</p>}
           </p>
           <p className="roadmap-child-container__category">{category}</p>
-          
         </div>
 
         <div className="roadmap-child-container__comment">
