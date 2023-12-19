8ad88a5d8b2f8e1d38772f53ab9dc027ee4cdf33
KasperPtak <kasperptak2@gmail.com>
2023-12-19 11:08:34 +0100

    updated fetch links to fit production

--- webdock-app/src/main.jsx
+++ webdock-app/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./global-styling/Global.scss"
 
-window.apiHostName = "http://localhost:1234/api";
+window.apiHostName = "http://http://kmfpgroup5.vps.webdock.cloud:1234/api";
 // window.apiHostName = "http://localhost:1234/api";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
