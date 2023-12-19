8eb7c700764a4d39d559683e35a4a9ab578e519b
KasperPtak <kasperptak2@gmail.com>
2023-12-19 09:30:34 +0100

    checkadmin now checks for either ucl or webdock email

--- webdock-app/src/helper/checkAdmin.js
+++ webdock-app/src/helper/checkAdmin.js
@@ -1,6 +1,6 @@
 export const checkAdmin = (userEmail) => {
   // if (userEmail.endsWith("@webdock.io" || "@edu.ucl.dk")) {
-  if (userEmail !== null && userEmail.endsWith("@edu.ucl.dk")) {
+  if (userEmail !== null && userEmail.endsWith("@edu.ucl.dk") || userEmail.endsWith("@webdock.io")) {
     return true;
   } else {
     return false;
--- webdock-app/src/main.jsx
+++ webdock-app/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./global-styling/Global.scss"
 
 window.apiHostName = "http://localhost:1234/api";
+// window.apiHostName = "http://localhost:1234/api";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
