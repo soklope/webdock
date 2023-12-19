7cd2cca24356dad27b1c820600cdc103163fe9da
KasperPtak <kasperptak2@gmail.com>
2023-12-19 12:58:02 +0100

    Updated links to fit production build

--- webdock-app/src/main.jsx
+++ webdock-app/src/main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./global-styling/Global.scss"
 
-// window.apiHostName = "http://kmfpgroup5.vps.webdock.cloud:1234/api";
+window.apiHostName = "http://kmfpgroup5.vps.webdock.cloud:1234/api";
 
-window.apiHostName = "http://localhost:1234/api";
+// window.apiHostName = "http://localhost:1234/api";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
